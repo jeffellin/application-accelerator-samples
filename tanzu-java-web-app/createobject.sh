9f8d048c8ad047af47f04323951c32c6f7327070
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-26 09:17:21 -0400

    add create object sript

---
+++ tanzu-java-web-app/createobject.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+SOURCE=$(mktemp -d)
+DEST=/Users/jeff/Downloads/demo
+COMPONENT_TYPE=$1
+PACKAGE_NAME=$2
+CLASS_NAME=$3
+
+JSON_STRING=$( jq -n -r \
+                  --arg cn "$CLASS_NAME" \
+                  --arg ct "$COMPONENT_TYPE" \
+                  --arg pn "$PACKAGE_NAME" \
+                  '{
+                    className : $cn, 
+                    componentType : $ct,
+                    includeTest : "yes",
+                    pkgName : $pn,
+                    projectName : "test-accelerator"}'
+                  )
+
+echo $JSON_STRING > test.txt
+
+
+tanzu acc generate test-accelerator --server-url https://accelerator.view.amer.end2end.link --options-file=test.txt
+
+unzip -q test-accelerator.zip -d $SOURCE 
+cd "${SOURCE}/test-accelerator";
+find . -type d  -print -exec mkdir -p "/Users/jeff/Downloads/demo/{}" \;
+find . -type f -exec cp {}  "/Users/jeff/Downloads/demo/{}" \;
+find . -type d -empty -delete
